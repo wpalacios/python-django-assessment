e383fea992dcfb889b874fe116d5c85c848520f8
Jonathan Guadamuz <guadamuzjj@gmail.com>
2020-03-07 12:56:26 -0600

    Add django 2.2 dependency

--- moviesapp/movies/urls.py
+++ moviesapp/movies/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = "movies"
 
 urlpatterns = [
     url(r'^$', view=views.MovieListView.as_view(), name='index'),
-    url(r'^(?P<isbn>[\d\-]+)/$', view=views.MovieDetailView.as_view(), name='detail'),
+    url(r'^(?P<id>[\d\-]+)/$', view=views.MovieDetailView.as_view(), name='detail'),
     url(r'^create/$', view=views.MovieCreateView.as_view(), name='create'),
-    url(r'^update/(?P<isbn>[\d\-]+)/$', view=views.MovieUpdateView.as_view(), name='update'),
-    url(r'^delete/(?P<isbn>[\d\-]+)/$', view=views.MovieDeleteView.as_view(), name='delete'),
+    url(r'^update/(?P<id>[\d\-]+)/$', view=views.MovieUpdateView.as_view(), name='update'),
+    url(r'^delete/(?P<id>[\d\-]+)/$', view=views.MovieDeleteView.as_view(), name='delete'),
 ]
