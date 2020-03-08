87a91914e073ef8685bb1525ea978ac652e95941
Jonathan Guadamuz <guadamuzjj@gmail.com>
2020-03-08 12:48:16 -0600

    Using pk over id in urls

--- moviesapp/movies/urls.py
+++ moviesapp/movies/urls.py
@@ -7,8 +7,8 @@ app_name = "movies"
 
 urlpatterns = [
     url(r'^$', view=views.MovieListView.as_view(), name='index'),
-    url(r'^(?P<id>[\d\-]+)/$', view=views.MovieDetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[\d\-]+)/$', view=views.MovieDetailView.as_view(), name='detail'),
     url(r'^create/$', view=views.MovieCreateView.as_view(), name='create'),
-    url(r'^update/(?P<id>[\d\-]+)/$', view=views.MovieUpdateView.as_view(), name='update'),
-    url(r'^delete/(?P<id>[\d\-]+)/$', view=views.MovieDeleteView.as_view(), name='delete'),
+    url(r'^update/(?P<pk>[\d\-]+)/$', view=views.MovieUpdateView.as_view(), name='update'),
+    url(r'^delete/(?P<pk>[\d\-]+)/$', view=views.MovieDeleteView.as_view(), name='delete'),
 ]
