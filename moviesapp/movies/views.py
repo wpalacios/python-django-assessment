010c3951ff2e3d0b223b9451c1581c5ff552837e
Jonathan Guadamuz <guadamuzjj@gmail.com>
2020-03-08 14:17:31 -0600

    Implement CRUD operations for movies

--- moviesapp/movies/views.py
+++ moviesapp/movies/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 from django.shortcuts import redirect
 from django.http import Http404
 from django.urls import reverse_lazy
+from django.contrib import messages
 
 from .models import Movie
 
@@ -26,16 +27,57 @@ class MovieDetailView(DetailView):
             return self.render_to_response(context)
         except Http404:
             return redirect('movies:index')
-            
 
 
 class MovieCreateView(CreateView):
     """Create a new movie."""
+    model = Movie
+    fields = ['title', 'year', 'rated', 'released_on', 'genre', 'director', 'plot']
+
+    def form_valid(self, form):
+        messages.success(self.request, 'The movie has been successfully created!')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'The creation has failed.')
+        return super().form_invalid(form)
 
 
 class MovieUpdateView(UpdateView):
     """Update the requested movie."""
+    model = Movie
+    fields = ['title', 'year', 'rated', 'released_on', 'genre', 'director', 'plot']
+
+    def get(self, request, *args, **kwargs):
+        try:
+            self.object = Movie.objects.get(pk=self.kwargs.get('pk'))
+            context = self.get_context_data(object=self.object)
+            return self.render_to_response(context)
+        except Http404:
+            return redirect('movies:index')
+
+    def form_valid(self, form):
+        messages.success(self.request, 'The movie has been successfully updated!')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'The update has failed.')
+        return super().form_invalid(form)
 
 
 class MovieDeleteView(DeleteView):
     """Delete the requested movie."""
+    model = Movie
+    redirect_url ='movies:index'
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        try:
+          self.object.delete()
+          messages.success(self.request, 'The movie has been successfully deleted!')
+        except:
+          messages.error(self.request, 'The deletion has failed.')
+
+        return redirect(self.redirect_url)
+
+
